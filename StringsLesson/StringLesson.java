d107c8c0db737e8fa5df8608336b38f2556425d4
Mikhail Kiselev <39037466+kiselevmv@users.noreply.github.com>
2025-01-09 18:25:52 +0200

    Error
    Error

--- StringsLesson/StringLesson.java
+++ StringsLesson/StringLesson.java
@@ -3,7 +3,8 @@
 class StringLesson {
 	public static void main(String[] args) 
 	{
-		String str = "Hello World!";
+		String str;
+		str = "Hello!";
 		// StringBuilder str = new StringBuilder("Hello");
 		// str.append(" world");
 		System.out.println(str);
