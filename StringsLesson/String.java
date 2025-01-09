1a05fcee893a308ff6c14f72a4d0c4afb96eb365
Mikhail Kiselev <39037466+kiselevmv@users.noreply.github.com>
2025-01-09 18:19:18 +0200

    New topic
    New topic

---
+++ StringsLesson/String.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class StringLesson {
+	public static void main(String[] args)
+	{
+		StringBuilder str = new StringBuilder("Hello");
+		str.append(" world");
+		System.out.println(str);
+	}
+}
