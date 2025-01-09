621604f35ecf3f4d1b7f7682ae217dda5fd0efad
Mikhail Kiselev <39037466+kiselevmv@users.noreply.github.com>
2025-01-09 18:24:22 +0200

    Strange error
    Strange error

---
+++ StringsLesson/StringLesson.java
@@ -0,0 +1,11 @@
+// import java.util.Scanner;
+
+class StringLesson {
+	public static void main(String[] args) 
+	{
+		String str = "Hello World!";
+		// StringBuilder str = new StringBuilder("Hello");
+		// str.append(" world");
+		System.out.println(str);
+	}
+}
